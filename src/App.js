179ef35266fba4e353dc4f2fbc4151cc3fd9cd8d
Pedro Rojas <pedro.rojas@paymenttools.com>
2021-09-22 12:11:37 +0200

    Add basic localization using i18n

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Trans, useTranslation } from "react-i18next";
+
+const lngs = {
+  en: { nativeName: "English" },
+  de: { nativeName: "Deutsch" },
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              key={lng}
+              style={{
+                fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
+              }}
+              type="submit"
+              onClick={() => i18n.changeLanguage(lng)}
+            >
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
+        <p>{t("sayHello")}</p>
+        <p>
+          <Trans t={t}>sayHello</Trans>
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
